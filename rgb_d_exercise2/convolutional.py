4c73b3cb5fbafd7f5cc513c5f492d870740dea75
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2016-11-21 22:27:36 +0100

    Trying out mnist script and tried to add simple CNN to convolutional.py

--- rgb_d_exercise2/convolutional.py
+++ rgb_d_exercise2/convolutional.py
@@ -22,10 +22,12 @@ from __future__ import print_function
 import argparse
 import gzip
 import os
+import random
 import sys
 import time
 
 import numpy
+import sklearn
 from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
@@ -141,28 +143,91 @@ def main(argv=None):  # pylint: disable=unused-argument
   # define your model here
   # TODO
 
+  def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+  def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+  def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+  def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+  
+
+  W_conv1 = weight_variable([5, 5, 1, 32])
+  b_conv1 = bias_variable([32])
+
+  x_image = tf.reshape(train_data_node, [-1,32,32,1])
+
+  h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+  h_pool1 = max_pool_2x2(h_conv1)
+
+  W_conv2 = weight_variable([5, 5, 32, 64])
+  b_conv2 = bias_variable([64])
+
+  h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+  h_pool2 = max_pool_2x2(h_conv2)
+
+  W_fc1 = weight_variable([8*8* 64, 1024])
+  b_fc1 = bias_variable([1024])
+
+  h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*64])
+  h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+  keep_prob = tf.placeholder(tf.float32)
+  h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+  W_fc2 = weight_variable([1024, 10])
+  b_fc2 = bias_variable([10])
+
+  y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+  y_conv = tf.Print(y_conv, [y_conv], message="Haaaallooooo ")
+
+  # TODO DONE
+
   # TODO
   # compute the loss of the model here
   # TODO
-  loss = 0
+  loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, train_labels_node))
 
   # TODO
   # then create an optimizer to train the model
   # HINT: you can use the various optimizers implemented in TensorFlow.
   #       For example, google for: tf.train.AdamOptimizer()
   # TODO
-
+  train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
   # TODO
   # Make sure you also define a function for evaluating on the validation
   # set so that you can track performance over time
   # TODO
+  correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(train_labels_node,1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+
+  sess.run(tf.initialize_all_variables())
+  input = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
   # Create a local session to run the training.
-  with tf.Session() as sess:
+  with tf.InteractiveSession() as sess:
     # TODO
     # Make sure you initialize all variables before starting the tensorflow training
     # TODO
+
+    y_conv = tf.Print(y_conv, [y_conv], message="Haaaallooooo ")
+    train_data_batch = train_data.next_batch(BATCH_SIZE)
+    train_labels_batch = train_labels.next_batch(BATCH_SIZE)
     
+    #sess.run(train_step, feed_dict={train_data_node: batch_xs, train_labels_node: batch_ys})
+    print(train_labels_node.eval())
+    sess.run(train_step, feed_dict={train_data_node: train_data_batch, train_labels_node: train_labels_batch})
+
+    # Test trained model
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: test_data, y_: test_labels}))
     # Loop through training steps here
     # HINT: always use small batches for training (as in SGD in your last exercise)
     # WARNING: The dataset does contain quite a few images if you want to test something quickly
@@ -171,18 +236,20 @@ def main(argv=None):  # pylint: disable=unused-argument
     # for step in max_steps:
     # Hint: make sure to evaluate your model every once in a while
     # For example like so:
-    #print('Minibatch loss: {}'.format(loss))
-    #print('Validation error: {}'.format(validation_error_you_computed)
+    if random.randint(1, 100) < 10:
+      print('Minibatch loss: {}'.format(loss))
+      print('Validation error: {}'.format(validation_error_you_computed))
     
     # Finally, after the training! calculate the test result!
     # WARNING: You should never use the test result to optimize
     # your hyperparameters/network architecture, only look at the validation error to avoid
     # overfitting. Only calculate the test error in the very end for your best model!
-    # if test_this_model_after_training:
-    #     print('Test error: {}'.format(test_error))
-    #     print('Confusion matrix:') 
-    #     # NOTE: the following will require scikit-learn
-    #     print(confusion_matrix(test_labels, numpy.argmax(eval_in_batches(test_data, sess), 1)))
+
+    if test_this_model_after_training:
+      print('Test error: {}'.format(test_error))
+      print('Confusion matrix:') 
+      # NOTE: the following will require scikit-learn
+      print(confusion_matrix(test_labels, numpy.argmax(eval_in_batches(test_data, sess), 1)))
     pass
 
 if __name__ == '__main__':
