Fixed standard deviation, removed pooling and added and changing epsilon sometimes.

visual_rl_exercise4/train_agent.py
@@ -58,11 +58,10 @@ def append_to_hist(state, obs):
     state[-1, :] = obs
 
 
-
 def getNetwork(state_node, batchsize, NUM_LABELS):
 
     def weight_variable(shape):
-        initial = tf.truncated_normal(shape, stddev=0.1)
+        initial = tf.truncated_normal(shape, stddev=1e-4)
         return tf.Variable(initial)
 
     def bias_variable(shape):
@@ -77,46 +76,43 @@ def getNetwork(state_node, batchsize, NUM_LABELS):
                               strides=[1, 2, 2, 1], padding='SAME')
 
     assert tf.shape(state_node).eval({})[0] == batchsize
-    x_image = tf.reshape(state_node, [tf.shape(state_node).eval({})[0],4,4,-1])
+    x_image = tf.reshape(state_node, [tf.shape(state_node).eval({})[0],4, 4, -1])#4,4,-1])
 
-    N0, N1, N2, N3 = 64, 128, 32, 1024
+    N0, N1, N2, N3 = 16, 32, 32, 16 #1024
     W_conv0 = weight_variable([5, 5, tf.shape(x_image).eval({})[-1], N0])
     b_conv0 = bias_variable([N0])
 
     h_conv0 = tf.nn.relu(conv2d(x_image, W_conv0) + b_conv0)
-    h_pool0 = max_pool_2x2(h_conv0)
-    #h_pool0 = x_image
+    #h_pool0 = max_pool_2x2(h_conv0)
+
     W_conv1 = weight_variable([5, 5, N0, N1])
     b_conv1 = bias_variable([N1])
 
-    h_conv1 = tf.nn.relu(conv2d(h_pool0, W_conv1) + b_conv1)
-    #h_conv1 = tf.nn.relu(conv2d(h_conv0, W_conv1) + b_conv1)
+    #h_conv1 = tf.nn.relu(conv2d(h_pool0, W_conv1) + b_conv1)
+    h_conv1 = tf.nn.relu(conv2d(h_conv0, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
     W_conv2 = weight_variable([5, 5, N1, N2])
     b_conv2 = bias_variable([N2])
 
-    #h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
+    #h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     #h_conv2_flat = tf.reshape(h_conv2, [tf.shape(h_conv1).eval({})[0], -1])#[-1, 8*8*N2])
-    #h_conv2_flat = tf.reshape(h_conv2, [batchsize, -1])#[-1, 8*8*N2])
-    h_pool2 = max_pool_2x2(h_conv2)
+    h_conv2_flat = tf.reshape(h_conv2, [batchsize, -1])#[-1, 8*8*N2])
+    #h_pool2 = max_pool_2x2(h_conv2)
     #h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*N2])
-    h_pool2_flat = tf.reshape(h_pool2, [batchsize, -1])
-    #print('midsize3', h_pool2_flat)
-
+    #h_pool2_flat = tf.reshape(h_pool2, [batchsize, -1])
     #W_fc1 = weight_variable([4*4* N2, N3])
-    #W_fc1 = weight_variable([tf.shape(h_conv2_flat).eval({})[-1], N3])
-    W_fc1 = weight_variable([tf.shape(h_pool2_flat).eval({})[-1], N3])
+    W_fc1 = weight_variable([tf.shape(h_conv2_flat).eval({})[-1], N3])
+    #W_fc1 = weight_variable([tf.shape(h_pool2_flat).eval({})[-1], N3])
     b_fc1 = bias_variable([N3])
 
     #h_pool2_flat = tf.reshape(h_pool2, [-1, 8*8*N2])
-    #h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
+    #h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     W_fc2 = weight_variable([N3, NUM_LABELS])
     b_fc2 = bias_variable([NUM_LABELS])
-
     y_conv = tf.matmul(h_fc1, W_fc2) + b_fc2
     return y_conv
 
@@ -189,13 +185,16 @@ sess.run(tf.initialize_all_variables())
 # this is just an example and you might want to change it
 steps = 1 * 10**6
 epi_step = 0
+epi_use_small = 10000
 nepisodes = 0
+PR_EPSILON = opt.action_epsilon_big
 
 state = sim.newGame(opt.tgt_y, opt.tgt_x)
 state_with_history = np.zeros((opt.hist_len, opt.state_siz))
 append_to_hist(state_with_history, rgb2gray(state.pob).reshape(opt.state_siz))
 next_state_with_history = np.copy(state_with_history)
 avg_loss, avg_siz, min_loss = 0, 0, float("inf")
+
 for step in range(steps):
     if state.terminal or epi_step >= opt.early_stop:
         epi_step = 0
@@ -206,11 +205,13 @@ for step in range(steps):
         state_with_history[:] = 0
         append_to_hist(state_with_history, rgb2gray(state.pob).reshape(opt.state_siz))
         next_state_with_history = np.copy(state_with_history)
+    if step > epi_use_small:
+        PR_EPSILON = opt.action_epsilon_small
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # TODO: here you would let your agent take its action
     #       remember
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    if step and random() < opt.action_epsilon:
+    if step and random() < PR_EPSILON:
         action = tf.argmax(Qfeed_ph, 1).eval({state_ph: state_history_batch})[-1]
     else:
         action = randrange(opt.act_num)