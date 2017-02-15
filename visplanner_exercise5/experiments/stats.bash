27d4254ec396b14726a20e9203efa2ce1bb5ea66
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2017-02-15 23:34:07 +0100

    updated statistics script

--- visplanner_exercise5/experiments/stats.bash
+++ visplanner_exercise5/experiments/stats.bash
@@ -3,6 +3,6 @@
 for experiment in *.txt;
 do 
     echo $experiment ;
-    cat $experiment | grep ^[01]. | python -c "import sys, numpy; acc_lists = zip(*map(lambda x: map(float, x.split(' ')), sys.stdin.read().split('\n')[:-1])); print(str.join('\n', (('mean: %.5f, max: %.5f, std: %.5f' % (numpy.mean(acc), numpy.max(acc), numpy.std(acc))) for acc in acc_lists)));" ;
+    cat $experiment | grep ^[01]. | python -c "import sys, numpy; acc_lists = zip(*map(lambda x: map(float, x.split(' ')), sys.stdin.read().split('\n')[:-1])); print(str.join('\n', (('%d tests, mean: %.5f, min: %.5f, max: %.5f, std: %.5f' % (numpy.size(acc), numpy.mean(acc), numpy.min(acc), numpy.max(acc), numpy.std(acc))) for acc in acc_lists)));" ;
 done
 
